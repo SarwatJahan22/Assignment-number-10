13dfda55a825abc475f23da4d19e0639b010330f
Sarwat Jahan <159925204+SarwatJahan22@users.noreply.github.com>
2024-07-06 16:55:54 +0500

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,10 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+// If you don’t have anything specific to write because your programs are too simple at this point, 
+// just add your name and the current date at the top of each program file. 
+// Then write one sentence describing what the program does.
+//author: [Sarwat Jahan]
+//date: [Thursday , May 2, 2024]
+//task 9:printing my favorite number.
+var favoriteNumber = 7;
+//revealing my favorite number in a message formate.
+console.log("My favorite number is ".concat(favoriteNumber, "."));
